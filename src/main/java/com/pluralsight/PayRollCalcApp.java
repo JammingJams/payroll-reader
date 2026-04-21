925f3439c559755979dfe9e2f55dcd6477903ab8
JammingJams <noahlogan20o00@gmail.com>
2026-04-21 15:02:36 -0400

    Added a file writer! It is identical to the reader!

--- src/main/java/com/pluralsight/PayRollCalcApp.java
+++ src/main/java/com/pluralsight/PayRollCalcApp.java
@@ -1,8 +1,6 @@
 package com.pluralsight;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -26,8 +24,10 @@ public class PayRollCalcApp {
 
                 //This code splits each line into seperate parts in an array
                 String[] tempArray = temp.split("\\|");
+
                 //This code creates a new object
                 Employee employee = new Employee();
+
                 try {
                     employee.setEmployeeId(Integer.parseInt(tempArray[0]));
                     employee.setName(tempArray[1]);
@@ -35,16 +35,34 @@ public class PayRollCalcApp {
                     employee.setPayRate(Double.parseDouble(tempArray[3]));
                 }
                 catch (NumberFormatException e) {
-                    System.out.println("Invalid information");
+                    System.out.println("Numbers are formatted incorrectly: Try checking the information you put in!");
                 }
                 //This code stores our object into our array with the .add
                 employeeList.add(employee);
             }
+            bufReader.close();
 
         }
+
         //This captures the Input and Output
         catch (IOException e) {
             e.printStackTrace();
+        }
+
+        try {
+            FileWriter fileWriter = new FileWriter("src/main/resources/payroll-sept-2023.cvs");
+            BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+
+            String text;
+            for(int i = 1; i < employeeList.toArray().length; i++) {
+                text = String.format("%d|%s|$%.2f\n", employeeList.get(i).getEmployeeId(), employeeList.get(i).getName(), employeeList.get(i).getGrossPay());
+                bufWriter.write(text);
+            }
+            bufWriter.close();
+        }
+        catch (IOException e) {
+            System.out.println("ERROR:  An unexpected error occurred");
+            e.getStackTrace();
         }
 
         //This code prints out all of the employee's information
